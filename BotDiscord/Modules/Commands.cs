24236f57cb63c33afe782f2904d972c36f1aa786
VincentPapin <vincent.papin@wanadoo.fr>
2021-02-28 10:25:41 +0100

    Création du bot discord

---
+++ BotDiscord/Modules/Commands.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.Rest;
+using Discord.WebSocket;
+
+
+namespace BotDiscord.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong!");
+        }
+
+        [Command("Salut")]
+        public async Task SalutAsync()
+        {
+            await ReplyAsync("Salut");
+        }
+
+        [Command("Rand")]
+        public async Task RandAsync()
+        {
+            Random random = new Random();
+            int nbre = random.Next(100);
+            await ReplyAsync($"Nombre aléatoire : {nbre}");
+        }
+
+        [Command("Users")]
+        public async Task UserAsync()
+        {
+            string roleList = "";
+            var roles = Context.Guild.Roles;
+            foreach (var role in roles)
+            {
+                roleList += role.Name + "\n";
+            }
+
+            int nbreUser = Context.Guild.MemberCount;
+
+            await Context.Channel.SendMessageAsync($"{nbreUser} utilisateurs, roles : \n" + roleList);
+        }
+
+
+        [Command("IP")]
+        public async Task IPAsync()
+        {
+            var host = await Dns.GetHostEntryAsync(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    await ReplyAsync($"IP : {ip.ToString()}");
+                }
+            }
+
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ BotDiscord/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace BotDiscord
+{
+    class Program
+    {
+        private DiscordSocketClient client;
+        private CommandService commands;
+
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+            commands = new CommandService();
+
+            client.Log += Log;
+
+            client.Ready += () =>
+            {
+                Console.WriteLine("je suis prêt");
+                return Task.CompletedTask;
+            };
+
+            await InstallCommandAsync();
+            await client.LoginAsync(TokenType.Bot,
+                Environment.GetEnvironmentVariable("DiscordToken", EnvironmentVariableTarget.User));
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommandAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage msg)
+        {
+            var message = msg as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!message.HasCharPrefix('!', ref argPos)) return;
+
+            var context = new SocketCommandContext(client, message);
+
+            var result = await commands.ExecuteAsync(context, argPos, null);
+
+            //Erreur
+            if (!result.IsSuccess)
+            {
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
